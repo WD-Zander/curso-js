45389520330bf660ab61514a91386ae5e198fc4f
unknown <santoyoisabella14@gmail.com>
2024-11-30 16:04:50 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,57 @@
+// // // alert("Hola");
+// let message = "Hola mundo 3";
+// let message2 = "Hola mundo 4";
+// alert(message);
+// alert(message2);
+
+// let number1 = 12;
+// let number2 = 21;
+// alert(number1 + number2);
+// let year = prompt("Ingrese el año:")
+// alert(year)
+
+// Variables
+// let MyName;
+// MyName = "josefino";
+// let MyAge = 15;
+// alert(MyName);
+// alert(MyAge);
+// let MyName ="josefino2",
+//     MyAge = 15;
+//     MyMessage= "Hello :)";
+// alert(MyName);
+// alert(MyAge);
+// alert(MyMessage);
+
+// Constantes
+// const MyBirthDay = "Apr-21-2014";
+// // alert(MyBirthDay);
+// // Tipos de datos
+// let MyAge2 = 33;
+// let IsActive = true;
+// let IsBlue = false;
+// let MyHeight = null;
+// let MyHeight2 = undefined;
+ 
+// // Ingreso de argumentos
+// let  year = prompt('Año actual:');
+// alert(year);
+// Comfirm
+// let isBoss = comfirm("Eres el jefe ?");
+// alert(isBoss);
+// Operadores matematicos
+// let Num1 = 10;
+// let Num2 = 20;
+// alert(Num1 + Num2);
+// alert(Num1 - Num2);
+// alert(Num1 * Num2);
+// alert(Num1 / Num2);
+// let  Num3 = 2;
+// Num3++;
+// alert(Num3);
+// Operadores de comparacion
+alert( 2 > 1 );
+alert( 2 < 1 );
+alert( 2 == 1 );
+alert( 2 != 1 );
+// Condicionales
